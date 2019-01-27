8a192f47d203799994348a4118a7f3c35a72a7aa
mxio <maitongxiong@gmail.com>
2019-01-26 21:19:29 -0600

    validated item input

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 //function to add item to list
 function addItem() {
-    $('#js-shopping-list-form').on('click', function(event) {
+    $('#js-shopping-list-form button').on('click', function(event) {
         event.preventDefault();
 
         let groceryItem = $('input').val();
-
-        let newList = $(`<li><span class="shopping-item">${groceryItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
         
-        $('.shopping-list').prepend(newList);
+        let newList = $(`<li><span class="shopping-item">${groceryItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+
+        if (groceryItem) {
+            $('.shopping-list').prepend(newList);
+        }
+        else {
+            return;
+        }
     })
 }
 
 //function to toggle item's checkmark status
 function toggleItem() {
-
+    
+    $().on('click', function(event) {
+        //.shopping-item__checked
+        if ($('span').text() === "check") {
+            $(this).toggle('.shopping-item__checked');
+        }
+    })
 }
 
 //function to delete item
