cf0dd8d53a9abfd7688c60cef92498b7ae70e357
mxio <maitongxiong@gmail.com>
2019-01-26 20:52:19 -0600

    add item feature working

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 //function to add item to list
 function addItem() {
-    $('.js-shopping-list-form').on('click', function(event) {
+    $('#js-shopping-list-form').on('click', function(event) {
         event.preventDefault();
 
         let groceryItem = $('input').val();
 
         let newList = $(`<li><span class="shopping-item">${groceryItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
         
+        $('.shopping-list').prepend(newList);
     })
-
-    $('.shopping-list').append()
 }
 
 //function to toggle item's checkmark status
