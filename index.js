3e6942fdd19643f3c194925e1ac1f61839064f47
mxio <maitongxiong@gmail.com>
2019-01-26 19:34:46 -0600

    added base Javascript file

---
+++ index.js
@@ -0,0 +1,16 @@
+//function to add item to list
+function addItem() {
+
+}
+
+//function to toggle item's checkmark status
+function toggleItem() {
+
+}
+
+//function to delete item
+function deleteItem() {
+    
+}
+
+$addItem();
