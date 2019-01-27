3848ae75499cf97dd19add5b1e333e5df3a81962
mxio <maitongxiong@gmail.com>
2019-01-26 20:40:48 -0600

    added code to add items to html list

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 //function to add item to list
 function addItem() {
+    $('.js-shopping-list-form').on('click', function(event) {
+        event.preventDefault();
 
+        let groceryItem = $('input').val();
+
+        let newList = $(`<li><span class="shopping-item">${groceryItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        
+    })
+
+    $('.shopping-list').append()
 }
 
 //function to toggle item's checkmark status
@@ -10,7 +19,9 @@ function toggleItem() {
 
 //function to delete item
 function deleteItem() {
-    
+
 }
 
-$addItem();
+$(addItem);
+$(toggleItem);
+$(deleteItem);
